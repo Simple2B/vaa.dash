9249b08422ec615065fbd169c27521c2401fc2d1
Dana <romaniuk.dana@gmail.com>
2021-03-12 19:44:33 +0200

    basic admin_page

--- app/admin_page.py
+++ app/admin_page.py
@@ -1,8 +1,9 @@
 from flask_admin import Admin
+from flask_admin.contrib import sqla
 from flask_admin.contrib.sqla import ModelView
 from flask_admin.base import BaseView, expose
-
-from app.models import User
+from flask_login import current_user
+from app.models import User, Dashboard, Role
 
 
 class MyView(BaseView):
@@ -11,13 +12,23 @@ class MyView(BaseView):
         return self.render("admin_page/index.html")
 
 
+class SecureModelView(sqla.ModelView):
+
+    form_excluded_columns = ["password"]
+
+    def is_accessible(self):
+        # return True
+        return current_user.is_authenticated and current_user.role != "admin"
+
+
 def init_admin(app, db):
     admin = Admin(
         app,
         name="Visual Approach Analytics",
         template_mode="bootstrap3",
-        # index_view=CustomAdminIndexView(name="Home", url="/admin"),
     )
     # admin.add_views(SecureModelView(User, db.session))
     admin.add_views(ModelView(User, db.session))
+    admin.add_views(ModelView(Role, db.session))
+    admin.add_views(ModelView(Dashboard, db.session))
     return admin
