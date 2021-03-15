fdedb983b927748d11312ec371cdf57b3dd263e8
Dana <romaniuk.dana@gmail.com>
2021-03-15 20:38:30 +0200

    list of actual dashboards on index page

--- app/views/main.py
+++ app/views/main.py
@@ -1,13 +1,15 @@
 from flask import render_template, Blueprint
 from flask_login import login_required
 
+from app.models import Dashboard
 
 main_blueprint = Blueprint("main", __name__)
 
 
 @main_blueprint.route("/")
 def index():
-    return render_template("index.html")
+    dashboards = Dashboard.query.all()
+    return render_template("index.html", dashboards=dashboards)
 
 
 @main_blueprint.route("/dashboard")
