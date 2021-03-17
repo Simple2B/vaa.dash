97f7133982f18a37354e0385de5ef1f4037836fa
Dana <romaniuk.dana@gmail.com>
2021-03-17 19:09:55 +0200

    change base template

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,4 @@
 # flake8: noqa F401
 from .user import User, AnonymousUser
-from .role import Role
+from .role import Role, association_table
 from .dashboard import Dashboard
-from .role import association_table
